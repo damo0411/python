deb58058629a99da3d482a3f13e4b5dbe5f78a95
damo0411 <damodaran951104@gmail.com>
2025-05-14 10:16:59 +0000

    Float in python

---
+++ Day1.Float.py
@@ -0,0 +1,34 @@
+num1 = 5.0
+num2 = 2.5
+
+#Addition
+
+add = num1 + num2
+print ("Addition: ", add)
+
+#Subtraction
+sub = num1 - num2
+print ("Subtraction:", sub)
+
+#Multiplication
+mul = num1 * num2
+print ("Multiplication:", mul)
+
+#Division
+div = num1 / num2
+print ("Division:", div)
+
+#Modulus
+mod = num1 % num2
+print ("Modulus:", mod) 
+
+#Round off
+num3 = 3.14569874166245
+round_num = round(num3, 4)
+print ("Round off:", round_num)
+
+#Power
+power = num1 ** num2        
+print ("Power:", power)
+
+

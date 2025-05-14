52bc401c11280723a12f2e41b9882718132c8da1
damo0411 <damodaran951104@gmail.com>
2025-05-14 09:47:11 +0000

    Strings in Python

---
+++ Day1.string.py
@@ -0,0 +1,26 @@
+str1="bomb"
+str2="blast"
+result = str1 + "" + str2
+print(result)
+
+text = "Monkey D Luffy"
+length = len(text)
+print("Length of string is:l", length)
+
+
+yup="Monkey D Garp"
+upper= yup.upper()
+lower= yup.lower()
+
+print ("Upper case string is:", upper)
+print ("Lower case string is:", lower)
+
+
+chef=" Sanji zoro"
+new_chef= chef.replace("Zoro", "Black foot")
+print("The Chef is:", new_chef)
+
+Swordsman = "Roronoa Zoro"
+Cut = Swordsman.split()
+print("The Swordsman is:", Cut)
+

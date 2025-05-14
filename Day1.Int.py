d9d7bf433e68dc72ae48622dd53fac84ac9ab7bb
damo0411 <damodaran951104@gmail.com>
2025-05-14 10:21:04 +0000

    Intergers

---
+++ Day1.Int.py
@@ -0,0 +1,16 @@
+# Integer variables
+num1 = 10
+num2 = 5
+
+# Integer Division
+result1 = num1 // num2
+print("Integer Division:", result1)
+
+# Modulus (Remainder)
+result2 = num1 % num2
+print("Modulus (Remainder):", result2)
+
+# Absolute Value
+num3= -205480515
+result3 = abs(num3)
+print("abs value:", result3)
